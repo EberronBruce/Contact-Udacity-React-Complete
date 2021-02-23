5968499b847a1e35e1f537f50684a843352794df
Bruce <bruce@redravencomputing.com>
2021-02-23 12:29:31 -0700

    Added contacts

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import React, { Component } from 'react';
 import ListContacts from  './ListContacts'
 
-const contact = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "@karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "@richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "@tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-];
 
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "@karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg",
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "@richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg",
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "@tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg",
+      },
+    ],
+  };
+
   render() {
     return (
       <div>
-        <ListContacts contacts={contact} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
-    )
+    );
   }
 }
 
